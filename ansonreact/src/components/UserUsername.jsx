780fb80c8d1bcec7520216df2ef6335175a62508
AndrewSecond <31904920+AndrewSecond@users.noreply.github.com>
2024-07-03 19:08:32 +0300

    Create UserUsername.jsx

---
+++ ansonreact/src/components/UserUsername.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserUsername() {
+    return (
+        <div>
+            <b>Username: </b>
+            <span></span>
+        </div>
+    )
+}
