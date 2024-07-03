63c84d7bcc7f9a95304c403b3fc0cedc4827efe7
AndrewSecond <31904920+AndrewSecond@users.noreply.github.com>
2024-07-03 20:19:15 +0300

    Update UserUsername.jsx

--- ansonreact/src/components/UserUsername.jsx
+++ ansonreact/src/components/UserUsername.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function UserUsername() {
+export default function UserUsername(props) {
     return (
         <div>
             <b>Username: </b>
-            <span></span>
+            <span>{props.username}</span>
         </div>
     )
 }
