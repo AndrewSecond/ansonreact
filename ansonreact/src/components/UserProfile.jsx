f2d7a0dc27de56f703d0a0f3881a23c9f7eba362
AndrewSecond <31904920+AndrewSecond@users.noreply.github.com>
2024-07-03 15:18:25 +0300

    Update UserProfile.jsx

--- ansonreact/src/components/UserProfile.jsx
+++ ansonreact/src/components/UserProfile.jsx
@@ -1,7 +1,14 @@
-
+import React from 'react';
+import UserFavoriteFoods from './UserFavoriteFoods';
 
 export default function UserProfile() {
   return (
-    
-  )
+        <div id="user-profile">
+            <div>
+                <span>Email: </span>
+                <span>example@gmaol.com</span>
+            </div>
+            <UserFavoriteFoods />
+        </div>
+    )
 }
