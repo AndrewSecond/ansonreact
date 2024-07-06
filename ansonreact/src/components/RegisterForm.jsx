3cc77ae8238966e9c6488aacd2961e22f8012d37
AndrewSecond <31904920+AndrewSecond@users.noreply.github.com>
2024-07-06 05:08:32 +0300

    Create RegisterForm.jsx

---
+++ ansonreact/src/components/RegisterForm.jsx
@@ -0,0 +1,54 @@
+import {useState} from 'react';
+
+export default function RegisterForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayname, setDisplayname] = useState("");
+
+  return (
+    <form method="POST" onSubmit={(e) => {
+      
+    }} >
+      <div>
+        <label htmlFor="username">Username</label>
+        <input 
+          type="text" 
+          id="username" 
+          name="username"
+          value={username}
+          onChange={(e) => { 
+            setUsername(e.target.value);
+          }} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <br />
+        <input 
+          type="text" 
+          id="password" 
+          name="password"
+          type="password" 
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }} />
+      </div>
+      <div>
+        <label htmlFor="diaplayname">Display Namme</label>
+        <input 
+          type="text" 
+          id="displayname" 
+          name="displayname"
+          value={displayname}
+          onChange={(e) => {
+            setDisplayname(e.target.value);
+          }} />
+      </div>
+      <button>Login</button>
+      <div><span>Username: {username}</span></div>  
+      <div><span>Password: {password}</span></div>  
+      <div><span>Display Name: {displayname}</span></div>  
+    </form>
+    
+  )
+}
